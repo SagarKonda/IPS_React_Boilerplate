194f1dc1f5c5ccde624da97558ff78c81a2a4baf
Sagar Konda <skonda@ips.local>
2019-06-27 09:56:39 -0400

    eslint chnages added --fix, moved lint files out of src

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,27 @@
-var path    = require('path');
-var hwp     = require('html-webpack-plugin');
+var path = require("path");
+var hwp = require("html-webpack-plugin");
 
 module.exports = {
-    entry: path.join(__dirname, '/src/index.js'),
-    output: {
-        filename: 'build.js',
-        path: path.join(__dirname, '/dist')
-    },
-    module:{
-        rules:[{
-            exclude: /node_modules/,
-            test: /\.(js|jsx)$/,
-            loader: 'babel-loader',
-            resolve: {
-                extensions: [".js", ".jsx"]
-            },
-        }]
-    },
-    resolve: {
-        extensions: ['', 'js', 'jsx'],
-        modules: [
-          'node_modules',
-        ],
-      },
-    plugins:[
-        new hwp({template:path.join(__dirname, '/public/index.html')})
+  entry: path.join(__dirname, "/src/index.js"),
+  output: {
+    filename: "build.js",
+    path: path.join(__dirname, "/dist")
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        resolve: {
+          extensions: [".js", ".jsx"]
+        }
+      }
     ]
-}
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+    modules: ["node_modules"]
+  },
+  plugins: [new hwp({ template: path.join(__dirname, "/public/index.html") })]
+};
